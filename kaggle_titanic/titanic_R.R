b560b30249b5972a22e8f6ec2dc5473f3ba0610f
bbest <bbest@nceas.ucsb.edu>
2015-08-13 17:34:13 -0700

    got through step 2 of R titanic Kaggle tutorial

---
+++ kaggle_titanic/titanic_R.R
@@ -0,0 +1,10 @@
+
+# Assign the training set
+train <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+
+# Assign the testing set
+test <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
+
+# Make sure to have a look at your training and testing set
+print(train)
+print(test)
